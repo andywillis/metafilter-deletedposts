0e24d288186364e49bfe40f2d054e2074bf0a9d9
Andy Willis <github@awillis.fastmail.fm>
2017-12-02 15:54:06 +0000

    First version

---
+++ README.md
@@ -0,0 +1,5 @@
+# metafilter-deletedposts
+
+Unofficial Metafilter web extension.
+
+A rewrite of the original Greasemonkey script [here](http://plutor.org/blog/code/greasemonkey/metafilter_deleted_posts/).
---
+++ content/index.js
@@ -0,0 +1,110 @@
+(function () {
+
+  function create(el) {
+    return document.createElement(el);
+  }
+
+  function qs(selector, context = document) {
+    return context.querySelector(selector);
+  }
+
+  function qsa(selector, context = document) {
+    return context.querySelectorAll(selector);
+  }
+
+  function insertBefore(newNode, refNode) {
+    refNode.parentNode.insertBefore(newNode, refNode);
+  }
+
+  function insertAfter(newNode, refNode) {
+    refNode.parentNode.insertBefore(newNode, refNode.nextSibling);
+  }
+
+  function init() {
+    const { posts, postIds } = getPostsAndIds();
+    const missingPostIds = getMissingPostIds(postIds);
+    insertMissingPosts(posts, missingPostIds);
+  }
+
+  init();
+
+  function insertMissingPosts(posts, missingPostIds) {
+    missingPostIds.forEach(function (id) {
+      const prev = qs(`[data-postid="${id - 1}"]`);
+      getDeletedPostHTML(id)
+        .then(data => data.text())
+        .then((html) => {
+          const { post, reason } = getContent(html);
+          const tempEl = create('div');
+          tempEl.classList.add('deletedpost', 'copy');
+          tempEl.innerHTML = insertLink(id, post);
+          tempEl.appendChild(getReason(reason));
+          insertAfter(tempEl, prev);
+          padDeletedPost(tempEl);
+        });
+    });
+  }
+
+  function insertLink(id, el) {
+    const commentsRe = /\d+ comments total/;
+    return el.innerHTML.replace(commentsRe, function (match) {
+      return `<a href="https://www.metafilter.com/${id}/">${match}</a>`;
+    });
+  }
+
+  function getReason(el) {
+    const tempEl = create('div');
+    tempEl.innerHTML = el.textContent;
+    tempEl.classList.add('deletedreason');
+    return tempEl;
+  }
+
+  function padDeletedPost(el) {
+    insertBefore(create('br'), el);
+    insertAfter(create('br'), el);
+  }
+
+  function getContent(html) {
+    const tempEl = create('div');
+    tempEl.innerHTML = html;
+    return { post: qs('.copy', tempEl), reason: qs('.reason', tempEl) };
+  }
+
+  function getDeletedPostHTML(id) {
+    return fetch(`https://www.metafilter.com/${id}/`);
+  }
+
+  function getPostsAndIds() {
+    const postIds = [];
+    const posts = getPosts();
+    posts.forEach(function (post) {
+      const postId = getPostId(post);
+      post.setAttribute('data-postid', postId);
+      postIds.push(postId);
+    });
+    return { posts, postIds };
+  }
+
+  function getMissingPostIds(postIds) {
+    const revPostIds = postIds.reverse();
+    const out = [];
+    for (let i = 1; i < revPostIds.length; i++) {
+      if (revPostIds[i] - revPostIds[i - 1] !== 1) {
+        out.push(revPostIds[i - 1] + 1);
+        revPostIds.splice(i, 0, revPostIds[i - 1] + 1);
+        i--;
+      }
+    }
+    return out;
+  }
+
+  function getPosts() {
+    return qsa('.post');
+  }
+
+  function getPostId(post) {
+    const favSpan = qs('span[id^=fav]', post);
+    return +favSpan.id.match(/fav1(\d+)/)[1];
+  }
+
+}());
